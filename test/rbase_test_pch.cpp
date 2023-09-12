c01ce327bb183f6e44712ce2a32f626b64989157
Milos Tosic <milostosic77@gmail.com>
2023-09-12 16:48:07 +0200

    Remove rapp as a dependency for unit tests

--- test/rbase_test_pch.cpp
+++ test/rbase_test_pch.cpp
@@ -5,31 +5,8 @@
 
 #include <rbase_test_pch.h>
 
-struct rbaseUnitTestApp : public rapp::App
+int main(int argc, char* argv[])
 {
-	RAPP_CLASS(rbaseUnitTestApp)
+	return UnitTest::RunAllTests();
+}
 
-		virtual ~rbaseUnitTestApp() {}
-
-	int init(int32_t, const char* const*, rtmLibInterface*)
-	{
-		return 0;
-	}
-
-	void suspend() {}
-	void resume() {}
-
-	void update(float)
-	{
-		UnitTest::RunAllTests();
-		quit();
-	}
-
-	void draw() {}
-
-	void shutDown()
-	{
-	}
-};
-
-RAPP_REGISTER(rbaseUnitTestApp, "rbaseUnitTest", "rbase Unit testing");
