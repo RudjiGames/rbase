f560ca3aa79e96ed8e35c33278c90b05fa51286f
Milos Tosic <milos@rudji.com>
2026-03-07 22:42:39 +0100

    Improve documentation

--- inc/path.h
+++ inc/path.h
@@ -11,121 +11,121 @@
 
 namespace rtm {
 
-	/// Retrieves file name with extension if it is a file path.
+	/// Retrieves file name with extension from a file path.
 	///
-	/// @param[in] _path       : File system path
+	/// @param[in] _path       : File system path.
 	///
-	/// @returns the pointer to file name portion of the path, nullptr if dir.
+	/// @returns pointer to file name portion of the path, or nullptr if the path is a directory.
 	const char* pathGetFileName(const char* _path);
 
-	/// Retrieves file name with extension if it is a file path.
+	/// Retrieves file name with extension from a file path and copies it into a buffer.
 	///
-	/// @param[in] _path       : File system path
-	/// @param[out] _buffer    : Buffer to store file name to
-	/// @param[in] _bufferSize : Size of the provided buffer
+	/// @param[in] _path       : File system path.
+	/// @param[out] _buffer    : Buffer to store the file name.
+	/// @param[in] _bufferSize : Size of the provided buffer, in bytes.
 	///
-	/// @returns true on success.
+	/// @returns true on success, false if the path has no file name or the buffer is too small.
 	bool pathGetFileName(const char* _path, char* _buffer, uint32_t _bufferSize);
 
-	/// Retrieves file name without extension if it is a file path.
+	/// Retrieves file name without extension from a file path.
 	///
-	/// @param[in] _path       : File system path
-	/// @param[out] _buffer    : Buffer to store file name to
-	/// @param[in] _bufferSize : Size of the provided buffer
+	/// @param[in] _path       : File system path.
+	/// @param[out] _buffer    : Buffer to store the file name (without extension).
+	/// @param[in] _bufferSize : Size of the provided buffer, in bytes.
 	///
-	/// @returns true on success.
+	/// @returns true on success, false if the path has no file name or the buffer is too small.
 	bool pathGetFilenameNoExt(const char* _path, char* _buffer, uint32_t _bufferSize);
 
-	/// Retrieves file extension if it is a file path.
+	/// Retrieves the file extension from a file path.
 	///
-	/// @param[in] _path       : File system path
+	/// @param[in] _path       : File system path.
 	///
-	/// @returns pointer to extension part of the file name.
+	/// @returns pointer to the extension (including the dot), or nullptr if none.
 	const char* pathGetExt(const char* _path);
 
-	/// Retrieves file extension if it is a file path.
+	/// Retrieves the file extension from a file path and copies it into a buffer.
 	///
-	/// @param[in] _path       : File system path
-	/// @param[out] _buffer    : Buffer to store file name to
-	/// @param[in] _bufferSize : Size of the provided buffer
+	/// @param[in] _path       : File system path.
+	/// @param[out] _buffer    : Buffer to store the extension.
+	/// @param[in] _bufferSize : Size of the provided buffer, in bytes.
 	///
-	/// @returns true on success.
+	/// @returns true on success, false if there is no extension or the buffer is too small.
 	bool pathGetExt(const char* _path, char* _buffer, uint32_t _bufferSize);
 
-	/// Retrieves current working directory.
+	/// Retrieves the current working directory.
 	///
-	/// @param[out] _buffer    : Buffer to store file name to
-	/// @param[in] _bufferSize : Size of the provided buffer
+	/// @param[out] _buffer    : Buffer to store the directory path.
+	/// @param[in] _bufferSize : Size of the provided buffer, in bytes.
 	///
 	/// @returns true on success.
 	bool pathGetCurrentDirectory(char* _buffer, uint32_t _bufferSize);
 
-	/// Retrieves path to data root directory
+	/// Retrieves the path to the data root directory.
 	///
-	/// @param[out] _buffer    : Buffer to store file name to
-	/// @param[in] _bufferSize : Size of the provided buffer
+	/// @param[out] _buffer    : Buffer to store the directory path.
+	/// @param[in] _bufferSize : Size of the provided buffer, in bytes.
 	///
 	/// @returns true on success.
 	bool pathGetDataDirectory(char* _buffer, uint32_t _bufferSize);
 
-	/// Retrieves path to data root directory.
+	/// Appends a sub-path to a base path.
 	///
-	/// @param[in] _path       : File system path to be appended to
-	/// @param[in] _appendPath : File system path to be appended
-	/// @param[out] _buffer    : Buffer to store resulting path to
-	/// @param[in] _bufferSize : Size of the provided buffer
+	/// @param[in] _path       : Base file system path.
+	/// @param[in] _appendPath : Path segment to append.
+	/// @param[out] _buffer    : Buffer to store the resulting combined path.
+	/// @param[in] _bufferSize : Size of the provided buffer, in bytes.
 	///
-	/// @returns true on success.
+	/// @returns true on success, false if the buffer is too small.
 	bool pathAppend(const char* _path, const char* _appendPath, char* _buffer, uint32_t _bufferSize);
 
-	/// Goes 'up' one level 
+	/// Navigates one directory level up from the given path.
 	///
-	/// @param[in] _path       : File system path
-	/// @param[out] _buffer    : Buffer to store resulting path to
-	/// @param[in] _bufferSize : Size of the provided buffer
+	/// @param[in] _path       : File system path.
+	/// @param[out] _buffer    : Buffer to store the parent directory path.
+	/// @param[in] _bufferSize : Size of the provided buffer, in bytes.
 	///
-	/// @returns true on success.
+	/// @returns true on success, false if already at the root or the buffer is too small.
 	bool pathUp(const char* _path, char* _buffer, uint32_t _bufferSize);
 
-	/// Removes relative portions of the path.
+	/// Resolves '.' and '..' segments and produces a canonical path.
 	///
-	/// @param[in] _path       : File system path
-	/// @param[out] _buffer    : Buffer to store resulting path to
-	/// @param[in] _bufferSize : Size of the provided buffer
+	/// @param[in] _path       : File system path to canonicalize.
+	/// @param[out] _buffer    : Buffer to store the resulting canonical path.
+	/// @param[in] _bufferSize : Size of the provided buffer, in bytes.
 	///
-	/// @returns true on success.
+	/// @returns true on success, false if the buffer is too small.
 	bool pathCanonicalize(const char* _path, char* _buffer, uint32_t _bufferSize);
 
-	/// Removes relative portions of the path
+	/// Resolves '.' and '..' segments in place.
 	///
-	/// @param[in] _path       : File system path
-    void pathCanonicalize(char* _path);
+	/// @param[in,out] _path   : File system path to canonicalize (modified in place).
+	void pathCanonicalize(char* _path);
 
-	/// Makes relative path absolute by appending it to base and canonicalizing it.
+	/// Converts a relative path to an absolute path by combining it with a base and canonicalizing.
 	///
-	/// @param[in] _relative   : Relative path to make absolute
-	/// @param[in] _base       : Base path
-	/// @param[out] _buffer    : Buffer to store resulting path to
-	/// @param[in] _bufferSize : Size of the provided buffer
+	/// @param[in] _relative   : Relative path to make absolute.
+	/// @param[in] _base       : Base directory path.
+	/// @param[out] _buffer    : Buffer to store the resulting absolute path.
+	/// @param[in] _bufferSize : Size of the provided buffer, in bytes.
 	///
-	/// @returns true on success.
+	/// @returns true on success, false if the buffer is too small.
 	bool pathMakeAbsolute(const char* _relative, const char* _base, char* _buffer, uint32_t _bufferSize);
 
-	/// Makes path relative.
+	/// Computes a relative path from one location to another.
 	///
-	/// @param[in] _pathFrom   : Path to be relative from
-	/// @param[in] _pathTo     : Path to be relative to
-	/// @param[out] _buffer    : Buffer to store resulting path to
-	/// @param[in] _bufferSize : Size of the provided buffer
+	/// @param[in] _pathFrom   : Starting directory path.
+	/// @param[in] _pathTo     : Target path to reach.
+	/// @param[out] _buffer    : Buffer to store the resulting relative path.
+	/// @param[in] _bufferSize : Size of the provided buffer, in bytes.
 	///
-	/// @returns true on success.
+	/// @returns true on success, false if the buffer is too small.
 	bool pathMakeRelative(const char* _pathFrom, const char* _pathTo, char* _buffer, uint32_t _bufferSize);
 	
-	/// Checks if path is absolute.
+	/// Checks if a path is absolute.
 	///
-	/// @param[in] _path       : File system path
+	/// @param[in] _path       : File system path.
 	///
-	/// @returns true if path is absolue.
+	/// @returns true if the path is absolute.
 	bool pathIsAbsolute(const char* _path);
 
 	/// Checks if path is a directory.
@@ -135,14 +135,14 @@ namespace rtm {
 	/// @returns true if path is a directory.
 	bool pathIsDirectory(const char* _path);
 
-	/// Splits path and stores list of directories to _dirList.
+	/// Splits a path into its individual directory components.
 	///
-	/// @param[in] _path       : File system path to split into list of directories
-	/// @param[out] _numDirs   : Pointer to store number of directories in the path
-	/// @param[out] _dirList   : Buffer to store resulting path to
-	/// @param[in] _maxDirs    : Maximum number of directories to retrieve
+	/// @param[in] _path       : File system path to split.
+	/// @param[out] _numDirs   : Receives the number of directory components found.
+	/// @param[out] _dirList   : Array of StringView to receive each path component.
+	/// @param[in] _maxDirs    : Maximum number of entries in _dirList.
 	///
-	/// @returns true on success.
+	/// @returns true on success, false if _maxDirs is too small to hold all components.
 	bool pathSplit(const char* _path, uint32_t* _numDirs, StringView* _dirList, uint32_t _maxDirs);
 
 } // namespace rtm
