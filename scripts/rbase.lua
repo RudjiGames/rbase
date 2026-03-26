eb3f1183f8cf976310441842fac810f7a6fdcb93
Milos Tosic <milos@rudji.com>
2026-03-26 07:34:41 +0100

    Include paths for client projects

--- scripts/rbase.lua
+++ scripts/rbase.lua
@@ -3,6 +3,14 @@
 -- License: http://www.opensource.org/licenses/BSD-2-Clause
 --
 
+function projectExtraConfig_rbase() 
+	includedirs	{ path.join(projectGetPath("rbase"), "../") }
+end
+
+function projectExtraConfigExecutable_rbase() 
+	includedirs	{ path.join(projectGetPath("rbase"), "../") }
+end
+
 function projectAdd_rbase() 
 	addProject_lib("rbase")
 end
